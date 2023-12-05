ca782615f851f1bacfb8e25dbd1ce22a58e8d97c
Andrea Riley <andreariley@me.com>
2023-12-05 18:55:44 -0500

    form working

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,46 @@ import axios from 'axios';
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export default function App() {
-const [location, setLocation] = useState({display_name: ""});
-const [searchQuery, setSearchQuery] = useState('');
+  const [location, setLocation] = useState( "");
+  const [searchQuery, setSearchQuery] = useState('');
 
-function updateQuery(event) {
-  setSearchQuery(event.target.value);
-}
-
- async function getLocation() {
-  alert('getting')
-    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
-    const response = await axios.get(API);
-    setLocation({display_name: searchQuery});
-
-}
-
-    
+  function updateQuery(event) {
+    console.log(event)
+    setSearchQuery(event.target.value);
   }
 
-  return (
-    <Container>
-      <LocationForm 
-      onClick={getLocation} 
-      onChange={updateQuery}
-      show={""} 
-      searchQuery={searchQuery} />
-      <h2>The city is {location.display_name}</h2>
-    </Container>
-  );
-}
+console.log(API_KEY)
+  async function getLocation() {
+    // alert('getting')
 
+    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+    console.log('Before Axios Request', API);
+    try {
+      const response = await axios.get(API);
+      console.log('API Response:', response.data);
+      setLocation(response.data[0].display_name );
+    }
+   catch (error){
+   
+   
+    console.error('API Request Error:', error);
+   }
+  }
+// function changeCity(newCity) {
+//   getLocation(newCity)
+// }
+
+
+console.log(location)
+return (
+  <Container>
+    <LocationForm
+      handleChangeCity={getLocation}
+      updateQuery={updateQuery}
+   />
+    <h2>The city is {location}</h2>
+  </Container>
+);
+
+}
 
--- src/LocationForm.jsx
+++ src/LocationForm.jsx
@@ -3,15 +3,32 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 
 
-export default function LocationForm({onClick, onChange, searchQuery}) {
+
+export default function LocationForm({handleChangeCity, updateQuery}) {
+
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleChangeCity()
+    console.log(event.target.value)
+  }
+
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label> Where would you like to explore?</Form.Label>
-        <Form.Control type="location" placeholder="Enter Here" value={searchQuery} onChange={onChange} />
+        <Form.Control 
+        type="location" 
+        placeholder="Enter Here"
+        onChange={updateQuery} 
+        />
         <Form.Text className="text-muted">
         </Form.Text>
-        <Button  variant="primary" type="submit" onClick={onClick}>
+        <Button  
+        variant="primary" 
+        type="submit" 
+        onClick={handleChangeCity}>
           {/* /* figure out where this onClick goes(here or App) */ }
           Explore!
         </Button>
